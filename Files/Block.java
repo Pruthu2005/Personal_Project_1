92bb1c5d9475064e281817c6e90bae5963577c1c
pChe0087 <pche0087@student.monash.edu>
2026-01-10 15:15:44 +1100

    Made the first block basic class that will make up the block chain

--- Files/Block.java
+++ Files/Block.java
@@ -1,5 +1,14 @@
 import java.util.Date;
 
 public class Block {
+    public String hash;
+    public String preHash;
+    private String data;
+    private long time;
 
+    public Block(String data, String preHash){
+        this.data = data;
+        this.preHash = preHash;
+        this.time = new Date().getTime();
+    }
 }
