eb4a050056318ae0ac2e12460ac2ace846912e76
pChe0087 <pche0087@student.monash.edu>
2026-01-19 14:23:12 +1100

    Changed the project to a maven project and installed and imported the Gson library. This basically created by google to allows conversion between java objects and JSON objects in both directions.

--- Files/Chain.java
+++ Files/Chain.java
@@ -1,13 +1,15 @@
+import java.util.ArrayList;
+import com.google.gson.*;
 
 public class Chain {
+
+
     public static void main(String[] args){
         Block block1 = new Block("First Block","0");
-        System.out.println("ID for block 1: " + block1.blockId);
+
 
         Block block2 = new Block("Second Block", block1.blockId);
-        System.out.println("ID for block 2: " + block2.blockId);
 
         Block block3 = new Block("Third Block", block2.blockId);
-        System.out.println("ID for block 3: " + block3.blockId);
     }
 }
