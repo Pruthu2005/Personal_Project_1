be20899563f5de1995f7496dce83939854adb502
pChe0087 <pche0087@student.monash.edu>
2026-01-20 12:55:45 +1100

    Added the blocks to a "block chain" and the Gson library will ensure that the block chain will look like what a block chain is supposed to look like

--- Files/Chain.java
+++ Files/Chain.java
@@ -2,14 +2,19 @@ import java.util.ArrayList;
 import com.google.gson.*;
 
 public class Chain {
-
+    public static ArrayList<Block> chain = new ArrayList<>();
 
     public static void main(String[] args){
         Block block1 = new Block("First Block","0");
-
+        chain.add(block1);
 
         Block block2 = new Block("Second Block", block1.blockId);
+        chain.add(block2);
 
         Block block3 = new Block("Third Block", block2.blockId);
+        chain.add(block3);
+
+        String chainJson = new GsonBuilder().setPrettyPrinting().create().toJson(chain);
+        System.out.println(chainJson);
     }
 }
