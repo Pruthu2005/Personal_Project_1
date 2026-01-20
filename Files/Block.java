87dc144a67372ab49c9010f5e6c75a5832cbac05
pChe0087 <pche0087@student.monash.edu>
2026-01-20 13:09:47 +1100

    Added a mining method. This mines the blocks by conducting the proof of work for the block to ensure that the block is valid and prevents unauthorised accesses

--- Files/Block.java
+++ Files/Block.java
@@ -5,6 +5,7 @@ public class Block {
     public String preBlockID;
     private String data;
     private long time;
+    private int zero;
 
     public Block(String data, String preHash){
         this.data = data;
@@ -16,5 +17,14 @@ public class Block {
     public String calcBlockID(){
         String calcBlockID = Utilities.cryptohelp(preBlockID + Long.toString(time) + data);
         return calcBlockID;
+    }
+
+    public void mining(int zeros){
+        String goal = new String (new char[zeros]).replace('\0','0');
+        while (!blockId.substring(0,zeros).equals(goal)){
+            zero += 1;
+            blockId = calcBlockID();
+        }
+        System.out.println("Block has been mined. Block ID: " + blockId);
     }
 }
