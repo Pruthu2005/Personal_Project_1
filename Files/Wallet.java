14eb73370b62150596109c59daee9ea46488254a
pChe0087 <pche0087@student.monash.edu>
2026-01-24 13:23:48 +1100

    Added a method called generateKeys which generates the public and private keys. The private key is essentially an indicator that the crytoCoin is the owners and intruders cannot spend this coin. Public keys are used for recieving payments and can be shared.

--- Files/Wallet.java
+++ Files/Wallet.java
@@ -1,9 +1,26 @@
 import java.security.*;
+import java.security.spec.ECGenParameterSpec;
 
 
 public class Wallet {
     public PrivateKey privateKey;
     public PublicKey publicKey;
 
+    public Wallet(){
+        this.generateKeys();
+    }
 
+    public void generateKeys() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA", "BC");
+            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecGenParameterSpec = new ECGenParameterSpec("prime192v1");
+            keyPairGenerator.initialize(ecGenParameterSpec,secureRandom);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
 }
