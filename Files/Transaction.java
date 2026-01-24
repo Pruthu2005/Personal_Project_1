47ec83fab90de3b7ada8ee3b1f1c5b075c2d56e7
pChe0087 <pche0087@student.monash.edu>
2026-01-24 13:38:58 +1100

    Made new Transaction class which will house all of the transactions in the blockChain. A constructor has been built where the transaction information such as sender, receiver, amount will be stored.

---
+++ Files/Transaction.java
@@ -0,0 +1,31 @@
+import ChainEssentials.Utilities;
+import com.sun.tools.javac.util.StringUtils;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+
+public class Transaction {
+
+    public String TransactionID;
+    public PublicKey senderID;
+    public PublicKey receiverID;
+    public float amount;
+    public byte[] identifier;
+
+    public ArrayList inputs = new ArrayList<>();
+    public ArrayList outputs = new ArrayList<>();
+
+    public static int generatedSeq = 0;
+
+    public Transaction(PublicKey sender, PublicKey receiver, float amount, ArrayList inputs){
+        this.senderID = sender;
+        this.receiverID = receiver;
+        this.amount = amount;
+        this.inputs = inputs;
+    }
+
+    private String TransactionHash(){
+        generatedSeq += 1;
+        return Utilities.cryptohelp(senderID.toString()+ receiverID.toString()+ Float.toString(amount));
+    }
+}
