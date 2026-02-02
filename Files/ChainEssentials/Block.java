fc3634f3a8c48c537bdedb6da35a111e31740d9a
pChe0087 <pche0087@student.monash.edu>
2026-02-02 16:38:19 +1100

    Added the transaction changes to the block so that the transactions are stored in the blocks

--- Files/ChainEssentials/Block.java
+++ Files/ChainEssentials/Block.java
@@ -1,16 +1,18 @@
 package ChainEssentials;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
     public String blockId;
     public String preBlockID;
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>();
     private String data;
     private long time;
     private int calc = 0;
 
-    public Block(String data, String preHash){
-        this.data = data;
+    public Block(String preHash){
         this.preBlockID = preHash;
         this.time = new Date().getTime();
         this.blockId = calcBlockID();
@@ -28,5 +30,20 @@ public class Block {
             blockId = calcBlockID();
         }
         System.out.println("ChainEssentials.Block has been mined. ChainEssentials.Block ID: " + blockId);
+    }
+
+    public boolean addTrans(Transaction transaction){
+        if (transaction == null){
+            return false;
+        }
+        if (preBlockID != "0"){
+            if (transaction.TransactionProcessing() == false){
+                System.out.println("Transaction Failed.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction successfully added to the block");
+        return true;
     }
 }
