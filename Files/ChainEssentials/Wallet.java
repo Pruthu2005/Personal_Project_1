063e426783793c43b208fb8672b5ba472bf39265
pChe0087 <pche0087@student.monash.edu>
2026-02-02 15:42:10 +1100

    Added two new methods:
    1: getBal: This method allows to get the balance of the user
    
    2: send: This method allows for the transactions to be generated

--- Files/ChainEssentials/Wallet.java
+++ Files/ChainEssentials/Wallet.java
@@ -2,11 +2,16 @@ package ChainEssentials;
 
 import java.security.*;
 import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Wallet {
     public PrivateKey privateKey;
     public PublicKey publicKey;
+
+    public HashMap<String, TransactionOutput> UTXOs = new HashMap<String, TransactionOutput>();
 
     public Wallet(){
         this.generateKeys();
@@ -24,5 +29,44 @@ public class Wallet {
         } catch (Exception e){
             throw new RuntimeException(e);
         }
+    }
+
+    public float getBal(){
+        float total_amount = 0;
+        for (Map.Entry<String,TransactionOutput> i :Chain.UTXOs.entrySet()){
+            TransactionOutput UTXO = i.getValue();
+            if (UTXO.Mine(publicKey)){
+                UTXOs.put(UTXO.ID, UTXO);
+                total_amount += UTXO.amount;
+            }
+        }
+        return total_amount;
+    }
+
+    public Transaction send(PublicKey _reciever, float value){
+        if (getBal() < value){
+            System.out.println("Insufficient funds. Transaction has been canceled");
+            return null;
+        }
+
+        ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+
+        float total_amount = 0;
+        for (Map.Entry<String, TransactionOutput> i: UTXOs.entrySet()){
+            TransactionOutput UTXO = i.getValue();
+            total_amount += UTXO.amount;
+            inputs.add(new TransactionInput(UTXO.ID));
+            if (total_amount > value){
+                break;
+            }
+        }
+
+        Transaction newTran = new Transaction(publicKey, _reciever, value,inputs);
+        newTran.genSig(privateKey);
+
+        for (TransactionInput i:inputs){
+            UTXOs.remove(i.transactionOutID);
+        }
+        return newTran;
     }
 }
