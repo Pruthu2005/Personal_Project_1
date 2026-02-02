a4e9cecdd233c06d341b37daccb1590ba2b14fac
pChe0087 <pche0087@student.monash.edu>
2026-02-02 16:39:08 +1100

    Adding a OGblock and OGtransaction for testing of the block chain.

--- Files/ChainEssentials/Chain.java
+++ Files/ChainEssentials/Chain.java
@@ -9,15 +9,29 @@ import com.google.gson.*;
 public class Chain {
     public static ArrayList<Block> chain = new ArrayList<>();
     public static HashMap<String,TransactionOutput> UTXOs = new HashMap<String, TransactionOutput>();
-    public static int zeros = 5;
+    public static int zeros = 3;
+    public static float minTransaction = 0.1f;
     public static Wallet wallet1;
     public static Wallet wallet2;
+    public static Transaction OGTransaction;
 
     public static void main(String[] args){
         Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 
         wallet1 = new Wallet();
         wallet2 = new Wallet();
+        Wallet coins = new Wallet();
+
+        OGTransaction = new Transaction(coins.publicKey, wallet1.publicKey, 100f,null);
+        OGTransaction.genSig(coins.privateKey);
+        OGTransaction.TransactionID = "0";
+        OGTransaction.outputs.add(new TransactionOutput(OGTransaction.receiverID, OGTransaction.amount, OGTransaction.TransactionID));
+        UTXOs.put(OGTransaction.outputs.get(0).ID,OGTransaction.outputs.get(0));
+
+        System.out.println("Creating and Mining Original block");
+        Block OGblock = new Block("0");
+        OGblock.addTrans(OGTransaction);
+        chain.add(OGblock);
 
         System.out.println("Private and Public keys: ");
         System.out.println(Utilities.getString(wallet1.privateKey));
@@ -30,17 +44,17 @@ public class Chain {
         System.out.println("Checking if transaction is valid: ");
         System.out.println(transaction.verifySig());
 
-        Block block1 = new Block("First ChainEssentials.Block","0");
+        Block block1 = new Block(OGblock.blockId);
         chain.add(block1);
         System.out.println("Mining block 1...");
         chain.get(0).mining(zeros);
 
-        Block block2 = new Block("Second ChainEssentials.Block", block1.blockId);
+        Block block2 = new Block(block1.blockId);
         chain.add(block2);
         System.out.println("Mining block 2...");
         chain.get(1).mining(zeros);
 
-        Block block3 = new Block("Third ChainEssentials.Block", block2.blockId);
+        Block block3 = new Block(block2.blockId);
         chain.add(block3);
         System.out.println("Mining block 3...");
         chain.get(2).mining(zeros);
@@ -72,6 +86,8 @@ public class Chain {
                 System.out.println("The block cannot be mined");
                 return false;
             }
+
+
         }
         return true;
     }
